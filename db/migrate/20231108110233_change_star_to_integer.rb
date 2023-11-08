a61f923330c85da9b08a3e0086400e09554a4347
MiyazakiAkari <mimrikaakizayami@gmail.com>
2023-11-08 22:10:02 +0900

    star1変更

--- db/migrate/20231108110233_change_star_to_integer.rb
+++ db/migrate/20231108110233_change_star_to_integer.rb
@@ -1,6 +1,11 @@
 class ChangeStarToInteger < ActiveRecord::Migration[7.1]
-  def change
-    change_column :reviews, :star, :integer, null: false
-    #Ex:- change_column("admin_users", "email", :string, :limit =>25)
+  def up
+    # 文字列を整数に変換するためのUSING句を追加
+    change_column :reviews, :star, 'integer USING CAST(star AS integer)'
+  end
+
+  def down
+    # ロールバックの場合は、整数を文字列に戻す
+    change_column :reviews, :star, :string
   end
 end
--- db/migrate/20231108110811_change_star.rb
+++
@@ -1,9 +0,0 @@
-class ChangeStar < ActiveRecord::Migration[7.1]
-  def up
-    change_column :reviews, :star, 'integer USING CAST(star AS integer)'
-  end
-
-  def down
-    change_column :reviews, :star, :string
-  end
-end
